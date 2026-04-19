c97a651fbaa24486d81334ced4e3add2836e6a53
mamu-hi700 <gbolahanteemy07@gmail.com>
2026-04-19 12:20:53 +0100

    added best of three logic

--- rock, paper, scissors.py
+++ rock, paper, scissors.py
@@ -1,64 +1,37 @@
 import random
 
-def get_computer_choice():
-    return random.choice(['rock', 'paper', 'scissors'])
+user_wins = 0
+computer_wins = 0
+options = ["rock", "paper", "scissors"]
 
-def determine_winner(user_choice, computer_choice):
-    if user_choice == computer_choice:
-        return 'draw'
-    elif(
-        (user_choice == 'rock' and computer_choice == 'scissors') or
-        (user_choice == 'scissors' and computer_choice == 'paper') or
-        (user_choice == 'paper' and computer_choice == 'rock')
-    ):
-        return 'win'
-    elif(
-        (computer_choice == 'rock' and user_choice == 'scissors') or
-        (computer_choice == 'scissors' and user_choice == 'paper') or
-        (computer_choice == 'paper' and user_choice== 'rock')
-    ):
-        return 'lose'
+print("--- Welcome to the Best of 3 Challenge! ---")
 
-def display_score(score):
-    print(f'score:Wins - {score['win']} , Losses- {score['lose']},Draw - {score['draw']}')
-    #score['win'] fetches the direct value from the dictionary
-
-def main():
-    print('---Welcome to the rock, paper, scissors game!!!---')
-    print('If you chose to quit type "quit" ')
-    print('Type reset to reset the game and scores ')
-
-    score = {'win': 0,
-             'lose': 0,
-             'draw': 0
-             }
-    while True:
-        user_choice = input('please enter your choice: ').lower()
-
-        if user_choice == 'quit':
-            print('---Thank you for playing---')
-            break
-        elif user_choice == 'reset':
-            print('---The game is now reset---')
-            score = {'win': 0,
-             'lose': 0,
-             'draw': 0
-             }
-            continue
-        elif user_choice not in ['rock', 'paper', 'scissors']:
-            print('---Enter a valid response!!!---')
-            continue
-
-        computer_choice = get_computer_choice()
-
-        print(f'You chose {user_choice}, the computer chose {computer_choice}')
-
-        result = determine_winner(user_choice,computer_choice)
-        print(f'You {result} this round')
-
-        score[result] += 1
-
-        display_score(score)
-
-main()
+while user_wins < 2 and computer_wins < 2:
+    user_input = input("Choose Rock, Paper, or Scissors (or Q to quit): ").lower()
     
+    if user_input == "q":
+        break
+    if user_input not in options:
+        print("Invalid move, try again.")
+        continue
+
+    computer_pick = random.choice(options)
+    print(f"Computer picked {computer_pick}.")
+
+    if user_input == computer_pick:
+        print("It's a tie!")
+    elif (user_input == "rock" and computer_pick == "scissors") or \
+         (user_input == "paper" and computer_pick == "rock") or \
+         (user_input == "scissors" and computer_pick == "paper"):
+        print("You won this round!")
+        user_wins += 1
+    else:
+        print("Computer won this round!")
+        computer_wins += 1
+
+    print(f"Score -> You: {user_wins} | Computer: {computer_wins}\n")
+
+if user_wins > computer_wins:
+    print("CONGRATULATIONS! You won the match!")
+else:
+    print("The computer won the match. Better luck next time!")
